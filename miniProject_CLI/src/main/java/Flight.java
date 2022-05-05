d029b405f5f0f7b85126bc440c103a1aa30908dd
sujangurung <sujangurung@sujangurungsmbp.home>
2022-05-05 09:42:04 +0100

    Classes added

--- miniProject_CLI/src/main/java/Flight.java
+++ miniProject_CLI/src/main/java/Flight.java
@@ -1,3 +1,46 @@
+import java.util.List;
+
 public class Flight {
+
+    //properties 
+private String flightId;
+private String destination;
+private List<Passenger> passengers;
+public static List<Flight> flights;
+
+    //constructor
+    public Flight(String flightId){
+        this.flightId = flightId;
+    }
+
+    //Getters
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public static List<Flight> getFlights() {
+        return flights;
+    }
+
+
+    //Method to add passengers
+    public void addPassenger(Passenger passenger){
+        passengers.add(passenger);
+    }
+
+    //Method to search destination
+    public Flight searchDestination(String destination){
+        return null;
+
+    }
+
 
 }
--- miniProject_CLI/src/main/java/Passenger.java
+++ miniProject_CLI/src/main/java/Passenger.java
@@ -1,4 +1,52 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.List;
+import java.util.Locale;
+
+
 public class Passenger {
+
+    //Properties
+    private String name;
+    private String contact;
+    private String passengerId;
+    private static List<Passenger> passengers;
+    public Flight flight;
+
+    //Constructor
+    public Passenger(String name, String contact){
+        this.name = name;
+        this.contact = contact;
+        createPassengerId();
+    }
+    //Method to create passengerId
+    public void createPassengerId(){
+
+        int length = 7;
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        this.passengerId = RandomStringUtils.random(length, useLetters, useNumbers).toUpperCase();
+
+    }
+    //Method to book a flight
+    public void bookFlight(Flight flight){
+        this.flight = flight;
+        System.out.println(this.name + " your flight " + flight.getFlightId()
+                            + " has been booked.");
+        System.out.println("Enjoy your holiday in " + flight.getDestination());
+    }
+
+
+    //Method to cancel a flight
+    public void cancelFlight(Flight flight){
+
+        flight.getPassengers().remove(this);
+        System.out.println(this.name + ", your flight " + flight.getFlightId()
+                           + " has been cancelled.");
+
+        // do you want to book another flight? boolean
+    }
+
 
 
 
